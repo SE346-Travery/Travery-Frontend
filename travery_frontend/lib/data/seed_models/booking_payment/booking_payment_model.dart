e46053273e04dfd34ca76e5dbc473df812572d06
buiducthien090605 <buiducthien090605@gmail.com>
2026-04-15 23:32:52 +0700

    Update booking_payment_model.dart

--- travery_frontend/lib/data/seed_models/booking_payment/booking_payment_model.dart
+++ travery_frontend/lib/data/seed_models/booking_payment/booking_payment_model.dart
@@ -45,7 +45,6 @@ class BookingPaymentModel {
   /// Tổng số khách
   int get totalGuests => adultCount + childCount;
 
-  /// Chuỗi mô tả số lượng khách
   String get guestDescription {
     if (adultCount > 0 && childCount > 0) {
       return '$adultCount Người lớn, $childCount Trẻ em';
